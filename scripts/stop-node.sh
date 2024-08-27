a025067fb7b2c80b986078eb7215b63926841f4f
Mike Clay <mike.clay@iohk.io>
2024-08-27 19:16:23 +0100

    update to stop script

--- scripts/stop-node.sh
+++ scripts/stop-node.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker ps --filter name=$1* --filter status=running -aq --filter status=restarting | xargs docker stop
+name=pc_${PWD##*/}
+cardano_node_name=${name}-cardano-node-1
+
+env NODE_TYPE=${PWD##*/} docker-compose -p $name down
+
+#docker ps --filter name=$1* --filter status=running -aq --filter status=restarting | xargs docker-compose down
